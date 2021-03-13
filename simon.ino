fe5e90f02473a525a215b6e3d3fe452a9db5aed5
NotQuiteAmish <calebhostetler@yahoo.com>
2021-03-13 14:12:08 -0500

    Add input code

--- simon.ino
+++ simon.ino
@@ -6,8 +6,8 @@
 static int NUM_LIGHTS = 4;
 static int SPEAKER_PIN = 5;
 
-int lightPins[4] = {13, 12, 11, 10};
-int buttonPins[4] = {9, 8, 7, 6};
+int lightPins[4] = {9, 8, 7, 6};
+int buttonPins[4] = {13, 12, 11, 10};
 // Frequencies for E4 A4 C#5 E5
 int tones[4] = {330, 440, 554, 659};
 
@@ -20,6 +20,7 @@ void setup() {
     buttons[i] = new Button(buttonPins[i]);
   }
   Serial.begin(9600);
+  randomSeed(analogRead(0) * analogRead(1));
 }
 
 void loop() {
@@ -31,15 +32,17 @@ void loop() {
   bool inGame = true;
   while(inGame){
     // Generate random number to be added to sequence
-    int next = rand() % NUM_LIGHTS;
+    int next = random(0, NUM_LIGHTS);
+    Serial.println(next);
     sequence[len] = next;
     len++;
 
     // Play the sequence of lights/tones
     // We don't care about any inputs during this so we can use delay();
     for (int i=0; i<len; i++){
-      digitalWrite(lightPins[sequence[i]], HIGH);
       tone(SPEAKER_PIN, tones[sequence[i]], 250);
+      digitalWrite(lightPins[sequence[i]], HIGH);
+      delay(250);
       digitalWrite(lightPins[sequence[i]], LOW);
       delay(250);
     }
@@ -58,6 +61,8 @@ void loop() {
         for (int i=0; i<NUM_LIGHTS; i++){
           if (buttons[i]->pressed()){
             input = i;
+            Serial.print(i);
+            Serial.println(" pressed");
             inputGiven = true;
           }
         }
@@ -65,7 +70,10 @@ void loop() {
       // Check if the given input was correct. 
       // If so, play the note and continue on to wait for the next input
       if ((inputGiven) and (input == sequence[responses])){
-        // TODO: Play note and show light, but allow it to be interrupted
+        tone(SPEAKER_PIN, tones[sequence[input]], 250);
+        digitalWrite(lightPins[input], HIGH);
+        delay(250);
+        digitalWrite(lightPins[input], LOW);
       }
       
       // If not,     
